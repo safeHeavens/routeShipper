e19dc14f4e3e1990982315def698f72275217312
Samson Belario <belariosammy@gmail.com>
2026-05-19 10:54:49 -0700

    Update layout.tsx

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from 'next'
 import { Montserrat } from 'next/font/google'
+import Script from 'next/script'
 import { Analytics } from '@vercel/analytics/next'
 import { Navbar } from '@/components/navbar'
 import { Footer } from '@/components/footer'
@@ -49,11 +50,33 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${montserrat.variable} font-sans antialiased`}>
         <Navbar />
+
         <main className="min-h-screen">
           {children}
         </main>
+
         <Footer />
         <Analytics />
+
+        {/* Smartsupp Live Chat */}
+        <Script id="smartsupp-chat" strategy="afterInteractive">
+          {`
+            var _smartsupp = _smartsupp || {};
+            _smartsupp.key = '65541a63715f84b5882f4d9938081010e528bfc3';
+
+            window.smartsupp || (function(d) {
+              var s, c, o = smartsupp = function() { o._.push(arguments) };
+              o._ = [];
+              s = d.getElementsByTagName('script')[0];
+              c = d.createElement('script');
+              c.type = 'text/javascript';
+              c.charset = 'utf-8';
+              c.async = true;
+              c.src = 'https://www.smartsuppchat.com/loader.js?';
+              s.parentNode.insertBefore(c, s);
+            })(document);
+          `}
+        </Script>
       </body>
     </html>
   )
